c46565d32219e5703d3eac4919f602b96e85d178
piilzner <nils.lofgren@hotmail.com>
2015-10-06 14:11:33 +0200

    favourites started

--- www/js/ctrl/favoritesCtrl.js
+++ www/js/ctrl/favoritesCtrl.js
@@ -2,4 +2,20 @@ app.controller('favoritesCtrl', ['$scope' ,function ($scope) {
  
     $scope.title = "Favorites view";
 
+    $scope.openTrash = function($event){
+
+    	// console.log(angular.element($event.target).parent());
+    	
+    	document.getElementById('delete-tab').style.right = "0%";
+
+    }
+
+    $scope.closeTrash = function($event){
+
+    	// console.log(angular.element($event.target).parent());
+    	
+    	document.getElementById('delete-tab').style.right = "-45%";
+
+    }
+
 }]);
