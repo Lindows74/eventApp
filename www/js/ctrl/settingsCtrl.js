9ebe98a3dd20bd570ee0fd15562afe6bdb34ecf5
Filip Ramstedt <magra@host-193-11-186-246.eduroam.bth.se>
2015-10-12 11:47:30 +0200

    added cities

--- www/js/ctrl/settingsCtrl.js
+++ www/js/ctrl/settingsCtrl.js
@@ -1,7 +1,13 @@
-app.controller('settingsCtrl', ['$scope' ,function ($scope) {
+app.controller('settingsCtrl', ['$scope', '$http', function ($scope, $http) {
  
     $scope.title = "Settings view";
     
     $scope.page = "settings";
+    
+    $scope.cities = [];
+    
+    $http.get("js/cities.json").then(function(data){
+        $scope.cities = data.data;
+    });
 
 }]);
