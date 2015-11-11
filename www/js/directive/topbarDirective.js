3a9b4d4da693c12d939df29ba6456ada04436512
piilzner <nils.lofgren@hotmail.com>
2015-11-11 20:23:33 +0100

    profile designed

--- www/js/directive/topbarDirective.js
+++ www/js/directive/topbarDirective.js
@@ -7,7 +7,22 @@ app.directive('topbar', function() {
         geo.getLocation().then(function(data){
           $scope.location = data;
         });
-        
+        $scope.init = function(){
+           $('.menu-btn').click(function(){
+               
+            if($('.side-menu').css("left") == "-150px"){
+
+			 $('.side-menu').css("left", "0px");
+
+		    }else{
+
+			 $('.side-menu').css("left", "-150px");
+
+		    }
+               
+               
+           }) 
+        }
     }],
     templateUrl: 'js/directive/template/topBar.html'
   };
